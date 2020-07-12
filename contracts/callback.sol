352ad8af1f5a69599c82ac75ffcdb69180935e5a
mikedeshazer <guyonaplane57@gmail.com>
2020-07-12 23:14:47 +0900

    added Keep callback

---
+++ contracts/callback.sol
@@ -0,0 +1,23 @@
+
+
+import "./IRandomKeepBeacon.sol";
+
+/**
+ * @title CallbackContract
+ * @dev Example callback contract for Random Beacon.
+ */
+contract CallbackContract is IRandomBeaconConsumer {
+
+    uint256 internal _lastEntry;
+
+    function __beaconCallback(uint256 entry)
+        public override
+    {
+        _lastEntry = entry;
+    }
+
+    function lastEntry() public view returns (uint256)
+    {
+        return _lastEntry;
+    }
+}
